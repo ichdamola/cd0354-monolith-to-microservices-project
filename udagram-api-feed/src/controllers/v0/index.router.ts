ed2bc680e7776c012943a5431e84429c24d81694
Ichdamola <adedadamola@gmail.com>
2022-10-27 04:53:57 +0100

    [UPD] remove User route

--- udagram-api-feed/src/controllers/v0/index.router.ts
+++ udagram-api-feed/src/controllers/v0/index.router.ts
@@ -3,7 +3,7 @@ import {FeedRouter} from './feed/routes/feed.router';
 
 const router: Router = Router();
 
-router.use('/users', UserRouter);
+router.use('/feed', FeedRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
